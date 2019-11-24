5818c505fed09fad3acf83921a15449ecaab4655
Shohei Kajihara <s.white.capybara@gmail.com>
2019-11-24 13:49:32 +0900

    mix new uiar

---
+++ lib/uiar.ex
@@ -0,0 +1,18 @@
+defmodule Uiar do
+  @moduledoc """
+  Documentation for Uiar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Uiar.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uiar_test.exs
@@ -0,0 +1,8 @@
+defmodule UiarTest do
+  use ExUnit.Case
+  doctest Uiar
+
+  test "greets the world" do
+    assert Uiar.hello() == :world
+  end
+end
